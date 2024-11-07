ea6f307f5195fdcb804573d820b5a2a868dd6b5d
Aditya <adityam874@gmail.com>
2024-11-07 09:43:14 +0530

    lesson 7 completed

---
+++ 7-Etherjs-and-Hardhat/contracts/MyContract.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier:MIT
+pragma solidity 0.8.7;
+
+contract MyContract {
+    uint num;
+
+    function increment() public {
+        num += 1;
+    }
+
+    function decrement() public {
+        num -= 1;
+    }
+    function getNum() public view returns (uint) {
+        return num;
+    }
+}
